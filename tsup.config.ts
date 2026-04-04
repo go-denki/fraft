9a4a60ece95ba4de2ecb631c2972b2f5e98e6cbd
Adrian Hideki <adrian.hideki@hotmail.com>
2026-04-04 19:11:36 -0300

    feat: add onSuccess hook to copy README.md to dist directory

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'tsup';
+import { copyFileSync } from "fs";
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
   treeshake: true,
+  async onSuccess() {
+    copyFileSync("README.md", "dist/README.md");
+  },
 });
