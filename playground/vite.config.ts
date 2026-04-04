ca1cb99a8412c18b7e474ec9f8ac56c469af2ea5
Adrian Hideki <adrian.hideki@hotmail.com>
2026-04-04 18:40:54 -0300

    feat: add conditions to resolve configuration and optimize dependencies

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -5,12 +5,16 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    conditions: ['module', 'import', 'default'],
     alias: {
       'node:fs': path.resolve('./src/stubs/node-fs.ts'),
       'node:path': path.resolve('./src/stubs/node-path.ts'),
       'fs': path.resolve('./src/stubs/node-fs.ts'),
       'path': path.resolve('./src/stubs/node-path.ts'),
     },
+  },
+  optimizeDeps: {
+    exclude: ['@go-denki/fraft'],
   },
   server: {
     port: 5173,
